3f7b47fabb27150140112405c4f7d41b89b25008
Rodrigo Miranda <rodrigo.mira08@gmail.com>
2025-03-01 09:22:36 -0300

    feat: add about page

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,97 @@
+import { Metadata } from 'next';
+import Image from 'next/image';
+import { getTranslations } from 'next-intl/server';
+
+import { getPathname, Link } from '@/i18n/routing';
+
+import GithubIcon from '@/components/brand-icons/github';
+import LinkedinIcon from '@/components/brand-icons/linkedin';
+import XIcon from '@/components/brand-icons/x';
+
+interface PageProps {
+  params: Promise<{ locale: string }>;
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'aboutPage' });
+
+  return {
+    title: t('metadata.title'),
+    description: t('metadata.description'),
+    alternates: {
+      canonical: getPathname({ locale, href: `/about` }),
+    },
+  };
+}
+
+function getAge(): number {
+  const diff = Date.now() - new Date('2002-11-27T23:02:00').getTime();
+  const age = new Date(diff);
+
+  return Math.abs(age.getUTCFullYear() - 1970);
+}
+
+const socialMedias = [
+  {
+    name: 'GitHub',
+    link: 'https://github.com/rodrigo-miranda18',
+    icon: GithubIcon,
+  },
+  {
+    name: 'Linkedin',
+    link: 'https://www.linkedin.com/in/rodrigoamiranda/',
+    icon: LinkedinIcon,
+  },
+  {
+    name: 'x',
+    link: 'https://x.com/ro_miranda27',
+    icon: XIcon,
+  },
+];
+
+export default async function AboutPage({ params }: PageProps) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'aboutPage' });
+
+  const age = getAge();
+
+  return (
+    <section className="mx-auto max-w-4xl px-6 pb-16 pt-20">
+      <h1 className="text-3xl font-semibold">About</h1>
+
+      <hr className="mt-6 border-border dark:border-secondary-border" />
+
+      <main className="mt-10 flex flex-col gap-8 md:flex-row">
+        <div className="flex basis-[37%] flex-col items-center text-center">
+          <Image className="rounded-full" src="/avatar.jpg" alt="avatar" width={192} height={192} />
+
+          <h2 className="mt-6 text-xl font-semibold">{t('name')}</h2>
+
+          <div className="mt-2 text-base leading-7 text-secondary-text">
+            {t('role')}
+            <br />
+            {t('bio')}
+          </div>
+
+          <div className="mt-6 flex gap-3">
+            {socialMedias.map((socialMedia) => (
+              <Link
+                key={socialMedia.name}
+                className="rounded-lg bg-background p-2 transition-colors hover:bg-background-hover"
+                href={socialMedia.link}
+                target="_blank"
+              >
+                {socialMedia.icon ? <socialMedia.icon className="h-6 w-6" /> : socialMedia.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <p className="basis-[63%] whitespace-pre-line leading-7 text-secondary-text">
+          {t('content', { age })}
+        </p>
+      </main>
+    </section>
+  );
+}
---
+++ src/components/brand-icons/x.tsx
@@ -0,0 +1,11 @@
+export default function XIcon({ className }: { className: string }) {
+  return (
+    <svg className={className} viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+      <path
+        xmlns="http://www.w3.org/2000/svg"
+        d="M22.0512 1.34515H26.3445L16.9645 12.0668L28 26.6537H19.3597L12.593 17.8057L4.84867 26.6537H0.553L10.5863 15.1853L0 1.34632H8.85967L14.9765 9.43365L22.0512 1.34515ZM20.545 24.0847H22.9238L7.567 3.77999H5.01433L20.545 24.0847Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
