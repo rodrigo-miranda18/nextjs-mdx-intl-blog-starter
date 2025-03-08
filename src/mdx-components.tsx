f83d61a0ed0f40756f4c3ed95f391c026465247c
Rodrigo Miranda <rodrigo.mira08@gmail.com>
2025-03-08 09:51:46 -0300

    feat: add image optimization for posts content images

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,26 @@
+import { ComponentPropsWithoutRef } from 'react';
 import type { MDXComponents } from 'mdx/types';
+import Image from 'next/image';
+import { cn } from './lib/utils/cn';
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-  };
+type ImageProps = ComponentPropsWithoutRef<'img'>;
+
+const components: MDXComponents = {
+  img: ({ src, alt, className, ...props }: ImageProps) => (
+    <Image
+      {...props}
+      className={cn('h-auto w-full', className)}
+      src={src || ''}
+      alt={alt || ''}
+      width={720}
+      height={492}
+      sizes="(min-width: 768px) 690px, (min-width: 1024px) 720px, 89vw"
+    />
+  ),
+};
+
+type MDXProvidedComponents = typeof components;
+
+export function useMDXComponents(): MDXProvidedComponents {
+  return components;
 }
